7bf6f2f7c8e05bb4a958389916882fb8b2420c36
Justin Wang <jlwang5@illinois.edu>
2020-02-29 15:52:19 -0600

    Refractor

---
+++ Modeling/main.py
@@ -0,0 +1,24 @@
+import baseline as b
+import capture_data as d
+import visualize as v
+import torch
+
+#Import data
+data = d.get_frames('vid.mp4').to('cuda:0')
+
+#Declare model
+model = b.Baseline().to('cuda:0')
+
+model.train(data, data, 100)
+model = torch.load('')
+
+real = v.gen_gif(data[:100].detach().cpu())
+real.save('real.gif')
+
+out = model.forward(data)
+
+recon = v.gen_gif(out[:100].detach().cpu())
+recon.save('recon.gif')
+
+
+
