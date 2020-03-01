ada3eefe66402fde0b82ac5f4d05bfa8df11d1e0
WeustiS <willceustis@gmail.com>
2020-02-29 21:12:00 -0600

    Adding sutff

--- prune.py
+++ prune.py
@@ -26,18 +26,7 @@ def printing(model):
             / float(model.conv3.conv.weight.nelement())
         )
     )
-    print(
-        "Sparsity in conv4.weight: {:.2f}%".format(
-            100. * float(torch.sum(model.conv4.conv.weight == 0))
-            / float(model.conv4.conv.weight.nelement())
-        )
-    )
-    print(
-        "Sparsity in deconv1.weight: {:.2f}%".format(
-            100. * float(torch.sum(model.deconv1.deconv.weight == 0))
-            / float(model.deconv1.deconv.weight.nelement())
-        )
-    )
+
     print(
         "Sparsity in deconv2.weight: {:.2f}%".format(
             100. * float(torch.sum(model.deconv2.deconv.weight == 0))
@@ -57,17 +46,19 @@ def printing(model):
         )
     )
 
-r = torch.rand(1, 3, 1920, 2160).to('cuda')
-r0 = torch.rand(1, 3, 1920, 2160).to('cuda')
+r = torch.rand(1, 3, 1920, 1080)
+r0 = torch.rand(1, 3, 1920, 1080)
 
 def measure(model, r):
-    t0 = time.time()
-    for i in range(25):
+    arr = [0]*9
+    for i in range(50):
         model(r)
-    return time.time()-t0
+    for i in range(10):
+        arr = [sum(x) for x in zip(arr, model(r))]
+    return [x/(sum(arr)) for x in arr], sum(arr)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = Baseline().to("cuda")
+model = Baseline()
 
 print("Unpruned time", measure(model, r), "\n")
 
@@ -78,10 +69,10 @@ parameters_to_prune = (
     (model.conv2.conv, 'bias'),
     (model.conv3.conv, 'weight'),
     (model.conv3.conv, 'bias'),
-    (model.conv4.conv, 'weight'),
-    (model.conv4.conv, 'bias'),
-    (model.deconv1.deconv, 'weight'),
-    (model.deconv1.deconv, 'bias'),
+    #(model.conv4.conv, 'weight'),
+    #(model.conv4.conv, 'bias'),
+    #(model.deconv1.deconv, 'weight'),
+    #(model.deconv1.deconv, 'bias'),
     (model.deconv2.deconv, 'weight'),
     (model.deconv2.deconv, 'bias'),
     (model.deconv3.deconv, 'weight'),
@@ -95,13 +86,13 @@ prune.global_unstructured(
     pruning_method=prune.L1Unstructured,
     amount=0.2,
 )
-print("Pruned time", measure(model, r0))
+print("Pruned time", measure(model, r))
 printing(model)
 print()
 prune.global_unstructured(
     parameters_to_prune,
     pruning_method=prune.L1Unstructured,
-    amount=0.7,
+    amount=0.8,
 )
-print("Pruned time", measure(model, r0))
+print("Pruned time", measure(model, r))
 printing(model)
---
+++ quantization.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import torchvision.transforms as transforms
+import torch.backends.quantized
+import os
+import time
+import sys
+import torch.quantization
+
+# # Setup warnings
+import warnings
+warnings.filterwarnings(
+    action='ignore',
+    category=DeprecationWarning,
+    module=r'.*'
+)
+warnings.filterwarnings(
+    action='default',
+    module=r'torch.quantization'
+)
+
+# Specify random seed for repeatable results
+torch.manual_seed(191009)
+
+qconfig = torch.quantization.get_default_qconfig('qnnpack')
+
+torch.backends.quantized.engine = 'qnnpack'
