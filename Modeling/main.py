77fec3ba4bc86794cba6080c87d4a1b3f7fcbd33
William Eustis <willceustis@gmail.com>
2020-03-01 07:43:33 +0000

    edits

--- Modeling/main.py
+++ Modeling/main.py
@@ -6,10 +6,10 @@ import torch
 #Declare model
 model = b.Baseline('cuda:0').to('cuda:0')
 
-#checkpoint = torch.load('checkpoint.pth')
-checkpoint = torch.load('checkpoint.pth')
+# checkpoint = torch.load('checkpoint.pth')
+# checkpoint = torch.load('checkpoint.pth')
 
-#model.do_train_on_vid('training_data/vid1', 100000, batch_size=4, checkpoint=checkpoint)
-model.do_train_on_vid('training_data/vid1', 100000, batch_size=4, checkpoint=checkpoint)
+# model.do_train_on_vid('training_data/vid1', 100000, batch_size=4, checkpoint=checkpoint)
+model.do_train_on_vid('training_data/vid1', 100000, batch_size=16)
 
 
